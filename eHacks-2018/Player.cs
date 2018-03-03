bbdb841ab5f686619e874b2d46a35cf69fd6bc80
Freakanoid <coconutcrash19@yahoo.com>
2018-03-03 16:50:38 -0600

    Yet another collision fix attempt

--- eHacks-2018/Player.cs
+++ eHacks-2018/Player.cs
@@ -8,10 +8,11 @@ namespace eHacks_2018
 	public class Player : Thing
 	{
 		private float hmax = 80f;
-		private float vmax = -360f;
+		private float vmax = 80f;
 		public int grounded; //0 on ground, 1 if else
 		public bool shootPressed;
 		public int facing; //1 facing right, -1 facing left
+		public int direction;
 		public int prevface;
 		public float hspeed; //horizontal speed
 		public float vspeed; //vertical speed
@@ -40,8 +41,32 @@ namespace eHacks_2018
 		{
 			controls.movementUpdate();
 			prevface = facing;
-			if (controls.left) { haccel = 1.0f; facing = -1; }
-			else if (controls.right) { haccel = 1.0f; facing = 1; }
+			if (controls.left) 
+			{
+				if (haccel > -1.0f)
+				{
+					haccel -= 0.3f;
+					if (haccel < -1.0f)
+					{
+						haccel = -1.0f;
+					}
+				}
+				facing = -1;
+				//haccel = haccel * facing;
+			}
+			else if (controls.right) 
+			{ 
+				if (haccel < 1.0f)
+				{
+					haccel += 0.3f;
+					if (haccel > 1.0f)
+					{
+						haccel = 1.0f;
+					}
+				}
+				facing = 1;
+				//haccel = haccel * facing;
+			}
 			//else { hspeed = 0f; }
 			if (controls.jump && grounded == 0) { jump(); }
 			if (controls.shoot && !shootPressed)
@@ -53,20 +78,35 @@ namespace eHacks_2018
 			{
 				shootPressed = false;
 			}
-			hspeed = haccel * hmax * facing;
+			hspeed = haccel * hmax;
 			vspeed = vaccel * vmax;
 			handleMove(gameTime, level);
-			vaccel = vaccel - 0.05f;
-			haccel -= 0.05f;
-			if (vaccel < 0.0f) { vaccel = 0.0f; }
-			if (haccel < 0.0f) { haccel = 0.0f; }
+			vaccel = vaccel + 0.25f;
+			//haccel -= 0.05f;
+			if (vaccel > 4.0f) 
+			{ 
+				vaccel = 4.0f; 
+			}
+			if (haccel < -0.05f) //if traveling left
+			{
+				haccel += 0.05f;
+			}
+			if (haccel > 0.05f) //if traveling right
+			{
+				haccel -= 0.05f;
+			}
+			if (haccel >= -0.05f && haccel <= 0.05f) //if completely slowed down
+			{
+				haccel = 0.0f;
+			}
+
 
 		}
 
 		public void handleMove(GameTime gameTime, Level level) 
 		{ 
 			position.X += hspeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-			position.Y += (vspeed + (160f * grounded)) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+			position.Y += (vspeed * grounded) * (float)gameTime.ElapsedGameTime.TotalSeconds;
 			colbox.X = position.X;
 			colbox.Y = position.Y;
 
@@ -74,25 +114,72 @@ namespace eHacks_2018
 			{
 				if (t.GetType().Equals(typeof(Wall)))
 				{
+					//grounded = 1;
 					if (colCheck(t.colbox))
 					{
+						grounded = groundCheck(t.colbox);
 						break;
 					}
+					//grounded = groundCheck(t.colbox);
 				}
+			}
+
+			foreach (Thing t in level.thingList)
+			{
 				if (t.GetType().Equals(typeof(Projectile)))
 				{
-					if (bulletCheck(t.colbox))
+					if (bullCheck(t.colbox))
 					{
+						//var temp = t.GetType().GetCustomAttributes(false);
+						//Projectile temp2 = (Projectile)temp[0];
+						Projectile p = t as Projectile;
+						if (p.speed < 0)
+						{
+							haccel -= 20f;
+							direction = -1;
+						}
+						else 
+						{ 
+							haccel += 20f;
+							direction = 1;
+						}
 						break;
 					}
 				}
 			}
 		}
 
-		public void groundCheck()
+		public int groundCheck(RectangleF rect)
 		{
 			//TODO
-			//grounded = true;
+			RectangleF temp = colbox;
+			temp.Height += 2;
+			//temp = RectangleF.Intersect(colbox, rect);
+			PointF tempPoint;
+			PointF tempPoint2;
+			tempPoint.X = temp.X;
+			tempPoint.Y = temp.Y + temp.Height;
+			tempPoint2.X = temp.X + temp.Width;
+			tempPoint2.Y = temp.Y + temp.Height;
+			if( (rect.Contains(tempPoint) && !temp.IsEmpty) ||
+			    (rect.Contains(tempPoint2) && !temp.IsEmpty) )
+			//if (temp.Width > temp.Height)
+			{
+				while (colbox.Y > rect.Y - 25)
+				{
+					colbox.Y--;
+				}
+				//colbox.Y = rect.Y - 25;
+				position.X = colbox.X;
+				position.Y = colbox.Y;
+				return 0;
+			}
+			else
+			{
+				position.X = colbox.X;
+				position.Y = colbox.Y;
+				return 1;
+			}
 		}
 
 		public bool colCheck(RectangleF rect)
@@ -123,6 +210,7 @@ namespace eHacks_2018
 			else if (temp.Width > temp.Height)
 			{
 				// up/down collision
+				vaccel = 0;
 				RectangleF temp2 = colbox;
 				temp2.Y += 1;
 				temp2 = RectangleF.Intersect(temp2, rect);
@@ -164,13 +252,14 @@ namespace eHacks_2018
 			return coll;
 		}
 
-		public bool bulletCheck(RectangleF rect)
+		public bool altCheck(RectangleF rect)
 		{
+			grounded = 1;
 			bool coll = false;
 			RectangleF temp = RectangleF.Intersect(this.colbox, rect);
-			//grounded = false;
+			//grounded = 1;
 
-			if (temp.Height > temp.Width)
+			if (temp.Height > temp.Width && !temp.IsEmpty)
 			{
 				// left/right collision
 				RectangleF temp2 = colbox;
@@ -179,40 +268,40 @@ namespace eHacks_2018
 				if (temp2.Width > temp.Width)
 				{
 					//left collision
-					haccel += .20f;
-					//colbox.X = rect.X - colbox.Width;
+					colbox.X -= 1;
 					coll = true;
 				}
 				else
 				{
 					//right collision
-					//colbox.X = rect.X + rect.Width;
-					haccel += .20f;
+					colbox.X += 1;
 					coll = true;
 				}
 			}
-			else if (temp.Width > temp.Height && !temp.IsEmpty)
+			else if (temp.Width > temp.Height)
 			{
 				// up/down collision
+				vaccel = 0;
 				RectangleF temp2 = colbox;
 				temp2.Y += 1;
 				temp2 = RectangleF.Intersect(temp2, rect);
 				if (temp2.Height > temp.Height)
 				{
 					//top collision
-					//colbox.Y = rect.Y - colbox.Height;
-					vaccel -= .20f;
-
+					colbox.Y -= 1;
 					coll = true;
-					//grounded = true;
+					grounded = 0;
 				}
 				else
 				{
 					//bottom collision
-					//colbox.Y = rect.Y + rect.Height;
-					vaccel += .20f;
+					colbox.Y += 1;
 					coll = true;
 				}
+			}
+			else if (temp.Width == temp.Height && !temp.IsEmpty)
+			{
+				colbox.Y -= 1;
 			}
 
 			position.X = colbox.X;
@@ -234,10 +323,21 @@ namespace eHacks_2018
 			return coll;
 		}
 
+		public bool bullCheck(RectangleF rect)
+		{
+
+			RectangleF temp = RectangleF.Intersect(this.colbox, rect);
+			if (temp.Width == 0 && temp.Height == 0)
+			{
+				return false;
+			}
+			else return true;
+		}
+
 		public void jump()
 		{
-			//TODO
-			vaccel = 1.0f;
+			vaccel = -6.0f;
+			grounded = 1;
 		}
 
 
