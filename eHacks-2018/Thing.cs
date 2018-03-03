faff70b7c5dad49e9332092b7e18c33cdab65d42
Unknown <coconutcrash19@yahoo.com>
2018-03-02 19:32:31 -0600

    Added class stubs for core layout.

---
+++ eHacks-2018/Thing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+namespace eHacks_2018
+{
+	public class Thing
+	{
+		protected Vector2 position; //Thing's position
+		protected RectangleF colbox; //collision box for Thing
+		public Thing()
+		{
+		}
+	}
+}
